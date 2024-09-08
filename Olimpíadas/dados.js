2eaace8b1df79138f188c70584bd59d7e650941f
Gustavo Henrique <guazambujinha@gmail.com>
2024-09-08 14:49:41 -0300

    atualizado

--- Olimpíadas/dados.js
+++ Olimpíadas/dados.js
@@ -55,6 +55,32 @@ const dados = [
         instagram: "https://www.instagram.com/thiagobrazpv/",
         tags: 'atletismo salto vara '
     },
-
+    {
+        titulo: 'Gustavo Borges',
+        descricao: `Gustavo Borges é um dos maiores nadadores da história do Brasil. Ele conquistou quatro medalhas olímpicas entre 1992 e 2004,
+                    se destacando nas provas de estilo livre e tornando-se um ícone da natação brasileira.`,
+        link: "https://pt.wikipedia.org/wiki/Gustavo_Borges",
+        instagram: "https://www.instagram.com/gustavo.borges/",
+        tags: 'natação natacao brasil olimpíadas medalhas'
+    },
+    {
+        titulo: 'Cesar Cielo',
+        descricao: `Cesar Cielo é o nadador brasileiro mais bem-sucedido em competições internacionais, com uma medalha de ouro nos Jogos Olímpicos de Pequim 2008
+                    nos 50 metros livres. Ele também é recordista mundial dessa prova, consolidando seu nome na história da natação.`,
+        link: "https://pt.wikipedia.org/wiki/Cesar_Cielo",
+        instagram: "https://www.instagram.com/cesarcielof/",
+        tags: 'natação natacao recorde ouro olimpíadas brasil'
+    },
+    {
+        titulo: 'Ítalo Ferreira',
+        descricao: `Ítalo Ferreira é um surfista brasileiro que fez história ao vencer a primeira medalha de ouro no surfe nas Olimpíadas de Tóquio 2020. 
+                    Com sua garra e talento, ele se consolidou como um dos maiores surfistas do mundo.`,
+        link: "https://pt.wikipedia.org/wiki/%C3%8Dtalo_Ferreira",
+        instagram: "https://www.instagram.com/italoferreira/",
+        tags: 'surf mar onda oceano prancha surfar italo'
+    },
+  
 ];
+
+
 
